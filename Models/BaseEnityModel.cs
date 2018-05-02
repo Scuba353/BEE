776d16c2098a51f7b5c8e9c03545b9ef3c9c76cd
Allyse Fischer <scuba353@gmail.com>
2018-05-02 16:15:31 -0700

    first commit

---
+++ Models/BaseEnityModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BEE.Models
+{
+    public abstract class BaseEntity {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string created_at { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public string updated_at { get; set; }
+    }
+}
---
+++ Models/BeeContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BEE.Models
+{
+    public class BeeContext : DbContext
+    {
+        // base() calls the parent class' constructor passing the "options" parameter along
+        public BeeContext(DbContextOptions<BeeContext> options) : base(options) { }
+        public DbSet<user> users { get; set; }
+        public DbSet<hive> hives { get; set; }
+        public DbSet<permission> permissions { get; set; }
+
+    }
+}
