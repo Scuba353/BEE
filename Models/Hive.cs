0c6d0b1c7a0fef5cc5847eca872bad199ccedc8c
RobbCrouse <robbcrouse@gmail.com>
2018-05-08 14:22:33 -0700

    Added Hive model and HiveViewModel

---
+++ Models/Hive.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEE.Models
+{
+    public class hive : BaseEntity
+    {
+        [Key]
+        public int hiveid { get; set; }
+        public string hiveAddress { get; set; }
+        public string hiveCity { get; set; }
+        public string hiveState { get; set; }
+        public int hiveZip { get; set; }
+        public string age { get; set; }
+        public string status { get; set; }
+        public string notes { get; set; }
+        
+        public DateTime createdAt { get; set; }
+        public DateTime updatedAt { get; set; }
+
+        
+        [ForeignKey("userid")]
+        public int userid { get; set; }
+
+
+
+        public user user { get; set; }
+
+
+    }
+}
